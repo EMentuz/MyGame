49a286038b5747fb21ca7970894db8fddb1cc0b3
EMentuz <mentuz1999@gmail.com>
2026-09-09 16:23:52 +0300

    draw in screen

--- main.py
+++ main.py
@@ -7,11 +7,28 @@ pygame.display.set_caption("Pygame EMentuz.corp")
 icon = pygame.image.load('images/icon.png') # иконка для окна
 pygame.display.set_icon(icon)
 
+square = pygame.Surface((50, 170)) # создание поверхности
+square.fill('Red')
+
+myfont = pygame.font.Font('fonts/CherryBombOne-Regular.ttf', 40) # создание шрифта
+text_surface = myfont.render("Mentuz's game", False, "Green") # дополнительные
+# характеристики к тестовой надписи: текстб цвеб задний фон сглаживание
+
+player = pygame.image.load('images/car.png') # иконка
+
 running = True
 while running:
 
+    screen.blit(square, (20, 40)) # вывод square на экран
 
     # screen.fill((45, 84, 142))  # цвет экрана
+
+    pygame.draw.circle(screen, "Blue", (600, 300), 30) # рисование на экране 2 способ
+    pygame.draw.circle(square, "Blue", (0, 0), 30) # рисование на фигуре
+
+    screen.blit(text_surface, (300, 100))# вывод текста на экран
+    screen.blit(player, (10, 10))# вывод player на экран
+
 
     pygame.display.update() # обновление экрана
 
