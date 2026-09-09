ae1c82b693af3af6e209ef1e6620326f125eb5e5
EMentuz <mentuz1999@gmail.com>
2026-09-09 15:32:20 +0300

    add icon

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ pygame.init() # инициализации игры
 screen = pygame.display.set_mode((600, 300)) # размеры экрана, flags=pygame.NOFRAME
 pygame.display.set_caption("Pygame EMentuz.corp")
 
+icon = pygame.image.load('images/icon.png') # иконка для окна
+pygame.display.set_icon(icon)
 
 running = True
 while running:
 
 
+    # screen.fill((45, 84, 142))  # цвет экрана
 
     pygame.display.update() # обновление экрана
 
@@ -16,3 +19,8 @@ while running:
         if event.type == pygame.QUIT: # кнопка выхода
             pygame.quit()
             running = False
+        elif event.type == pygame.KEYDOWN:# нажатие любой клавиши клавиатуры
+            if event.key == pygame.K_a: # нажатие клавиши а
+                screen.fill((10, 100, 150))  # цвет экрана
+
+
